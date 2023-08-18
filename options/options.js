23f0037dd609709ea74432531988d5352535d090
Crowdbotics <engineering@crowdbotics.com>
2023-08-18 10:47:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "skyhouse-dev-109981",
+  url: "https://skyhouse-dev-109981.botics.co",
+  api: "https://skyhouse-dev-109981.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
