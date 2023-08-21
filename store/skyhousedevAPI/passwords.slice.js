95b1348d9bf0c8b747385664b2320126726e0a8c
Ravinder QA Lead <ravinder.y@crowdbotics.com>
2023-08-21 16:46:33 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/skyhousedevAPI/passwords.slice.js
+++ store/skyhousedevAPI/passwords.slice.js
@@ -1,36 +1,39 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import { apiService } from "./api"
-export const rest_auth_password_reset_create = createAsyncThunk(
-  "passwords/rest_auth_password_reset_create",
-  async payload => {
-    const response = await apiService.rest_auth_password_reset_create(payload)
-    return response.data
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiService } from "./api";
+export const rest_auth_password_reset_create = createAsyncThunk("passwords/rest_auth_password_reset_create", async payload => {
+  const response = await apiService.rest_auth_password_reset_create(payload);
+  return response.data;
+});
+const initialState = {
+  entities: [],
+  api: {
+    loading: "idle",
+    error: null
   }
-)
-const initialState = { entities: [], api: { loading: "idle", error: null } }
+};
 const passwordsSlice = createSlice({
   name: "passwords",
   initialState,
   reducers: {},
   extraReducers: builder => {
-    builder
-      .addCase(rest_auth_password_reset_create.pending, (state, action) => {
-        if (state.api.loading === "idle") {
-          state.api.loading = "pending"
-        }
-      })
-      .addCase(rest_auth_password_reset_create.fulfilled, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.entities.push(action.payload)
-          state.api.loading = "idle"
-        }
-      })
-      .addCase(rest_auth_password_reset_create.rejected, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.api.error = action.error
-          state.api.loading = "idle"
-        }
-      })
+    builder.addCase(rest_auth_password_reset_create.pending, (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending";
+      }
+    }).addCase(rest_auth_password_reset_create.fulfilled, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities.push(action.payload);
+        state.api.loading = "idle";
+      }
+    }).addCase(rest_auth_password_reset_create.rejected, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error;
+        state.api.loading = "idle";
+      }
+    });
   }
-})
-export default { rest_auth_password_reset_create, slice: passwordsSlice }
+});
+export default {
+  rest_auth_password_reset_create,
+  slice: passwordsSlice
+};
